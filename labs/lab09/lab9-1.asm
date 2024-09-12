d7eca00ec615293b90e38fc9fcabcf249fcd8b70
tihasanov <1132226507@pfur.ru>
2024-09-12 18:15:32 +0300

    lab09

---
+++ labs/lab09/lab9-1.asm
@@ -0,0 +1,42 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите x: ',0
+result: DB 'f(g(x))=',0
+SECTION .bss
+x: RESB 80
+res: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+;------------------------------------------
+; Основная программа
+;------------------------------------------
+mov eax, msg
+call sprint
+mov ecx, x
+mov edx, 80
+call sread
+mov eax,x
+call atoi
+call _calcul ; Вызов подпрограммы _calcul
+mov eax,result
+call sprint
+mov eax,[res]
+call iprintLF
+call quit
+;------------------------------------------
+; Подпрограмма вычисления
+; выражения "2x+7"
+_calcul:
+call _subcalcul
+mov ebx,2
+mul ebx
+add eax,7
+mov [res],eax
+_subcalcul:
+mov ebx,3
+mul ebx
+sub eax,1
+ret
+
+ret ; выход из подпрограммы
